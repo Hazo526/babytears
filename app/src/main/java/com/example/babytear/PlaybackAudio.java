c1752dd2bae791b78300c2cf0d880923c2a44d4b
Nithya Krishnan <nithyaniti98@gmail.com>
2019-03-31 16:29:44 +0800

    Updated audio page, error on record, undone playback

---
+++ app/src/main/java/com/example/babytear/PlaybackAudio.java
@@ -0,0 +1,4 @@
+package com.example.babytear;
+
+public class PlaybackAudio {
+}
--- app/src/main/java/com/example/babytear/audiopage.java
+++ app/src/main/java/com/example/babytear/audiopage.java
@@ -8,14 +8,34 @@ import android.widget.Button;
 
 public class audiopage extends AppCompatActivity {
 
-    Button exit2btn;
+    Button exit2btn, recordbtn, playbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audiopage);
 
-        exit2btn = (Button)findViewById(R.id.exit2btn);
+        recordbtn = (Button)findViewById(R.id.record2btn);
+
+        recordbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent record2Intent = new Intent(getApplicationContext(), RecordAudio.class);
+                startActivity(record2Intent);
+            }
+        });
+
+        playbtn = (Button)findViewById(R.id.playback2btn);
+
+        playbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent play2Intent = new Intent(getApplicationContext(), PlaybackAudio.class);
+                startActivity(play2Intent);
+            }
+        });
+
+        exit2btn = (Button)findViewById(R.id.exitaudiobtn);
 
         exit2btn.setOnClickListener(new View.OnClickListener() {
             @Override
