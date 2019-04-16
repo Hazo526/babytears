387c09e15bc36191016b6501bcc34472e2e49672
Haziq Yusoff <haziq.yusoff.aw@gmail.com>
2019-04-16 23:32:48 +0800

    x

--- app/src/main/java/com/example/babytear/WhiteNoise.java
+++ app/src/main/java/com/example/babytear/WhiteNoise.java
@@ -1,11 +1,25 @@
 package com.example.babytear;
 
-import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class WhiteNoise extends AppCompatActivity {
     private Button whitenoiseplay, whitenoisesend, lullabyplay, lullabysend, choirplay, choirsend, whitenoisestop, lullabystop, choirstop;
@@ -24,9 +38,9 @@ public class WhiteNoise extends AppCompatActivity {
         choirsend = findViewById(R.id.choirsend);
         choirstop = findViewById(R.id.choirstop);
 
-        final MediaPlayer whitenoisesound= MediaPlayer.create(this, R.raw.whitenoise);
-        final MediaPlayer lullabysound= MediaPlayer.create(this, R.raw.lullaby);
-        final MediaPlayer choirsound= MediaPlayer.create(this, R.raw.choir);
+        final MediaPlayer whitenoisesound = MediaPlayer.create(this, R.raw.whitenoise);
+        final MediaPlayer lullabysound = MediaPlayer.create(this, R.raw.lullaby);
+        final MediaPlayer choirsound = MediaPlayer.create(this, R.raw.choir);
 
         whitenoiseplay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +53,13 @@ public class WhiteNoise extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 whitenoisesound.stop();
+            }
+        });
+
+        whitenoisesend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            /* */
             }
         });
 
@@ -69,8 +90,6 @@ public class WhiteNoise extends AppCompatActivity {
                 choirsound.stop();
             }
         });
-
-
 
 
 
